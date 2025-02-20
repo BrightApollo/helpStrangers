5f015d8da542fa8ea66db39bf54b44884d04e07b
BrightApollo <BrightApollo@users.noreply.github.com>
2025-02-20 09:54:34 -0500

    Exercises to do first (in numerical order)
    Solutions uploaded here, or email "ta@wanko.com".  Code must compile to be submitted for credit.  If help is needed, submit code with comments where the problems seem to occur.

---
+++ ch01_ex12_open.cpp
@@ -0,0 +1,24 @@
+/*
+12. A simple rule to estimate your ideal body weight is to allow 110 pounds for the
+first 5 feet of height and 5 pounds for each additional inch. Write a program with
+a variable for the height of a person in feet and another variable for the additional
+inches and input values for these variables from the keyboard. Assume the person
+is at least 5 feet tall. For example, a person that is 6 feet and 3 inches tall would be
+represented with a variable that stores the number 6 and another variable that stores
+the number 3. Based on these values calculate and output the ideal body weight.
+
+Sample output:
+
+    You must be at least five feet tall to run this program.
+    How tall are you in feet? 6
+    And how many inches? 1
+    Ideal bodyweight for someone 6 feet 1 inches is 175 pounds.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    return 0;
+}
